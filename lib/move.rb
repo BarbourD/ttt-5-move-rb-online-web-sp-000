6150d82146e18b89c67ffeac4fcb1820cbed3c83
David Barbour <danceguy303@gmail.com>
2020-08-22 03:50:50 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,9 +10,7 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-
-def move
-input_to_index
-
 board = [" ", " ", " ",]
+def move (board, index, value = "X")
+board[index] = value
 end
